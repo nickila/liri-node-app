1c4063d3d93b2d022dd3344958e3bf5d727edeb6
Brian Nickila <brian.nickila@gmail.com>
2018-11-20 23:24:56 -0600

    Deleted my key and secret code.

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var Spotify = require('node-spotify-api');
 
 
 var spotify = new Spotify({
-    id: "4c923cdc8bc6484e863caf9ba5ac1ecf",
-    secret: "91d4ae7b4be7428ebb42a5382a77ed6f"
+    id: "xxx",
+    secret: "xxx"
 });
 
 var inquirer = require("inquirer");
-// Your application should ask the user any five questions of your choosing.
-// The question set should include at least one:
 inquirer.prompt([
         {
         type: "input",
@@ -21,7 +19,7 @@ inquirer.prompt([
             console.log("You like " + inquirerResponse.band);
             
             
-            spotify.search({ type: 'artist', query: band, limit: 1 }, function(err, data) {
+            spotify.search({ type: 'artist', query: inquirerResponse.band, limit: 1 }, function(err, data) {
                 if (err) {
                   return console.log('Error occurred: ' + err);
                 }
@@ -37,12 +35,5 @@ inquirer.prompt([
         
 
     })
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data.name); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+
 
